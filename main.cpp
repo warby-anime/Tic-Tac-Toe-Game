0b1cacff075b58ad95b8bb87d99ee197e885fee9
warby-anime <warby.anime@gmail.com>
2025-06-09 12:39:51 +0100

    created grid

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main ( )
+{
+	std::cout << "  |  |  |  " << '\n';
+	std::cout << "__|__|__|__" << '\n';
+	std::cout << "  |  |  |  " << '\n';
+	std::cout << "__|__|__|__" << '\n';
+	std::cout << "  |  |  |  " << '\n';
+	std::cout << "  |  |  |  " << '\n';
+}
