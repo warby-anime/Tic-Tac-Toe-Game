ed92d1641c4e593a3d310323c2bc00780ac166bd
warby-anime <warby.anime@gmail.com>
2025-06-10 11:52:23 +0100

    Added a array of grid locations

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <string>
 
+void TicTacGrid()
+{
+	constexpr int TicTacCows{ 3 };
+	constexpr int TicTacRols{ 3 };
+	char loc[TicTacCows][TicTacRols]
+	{ { '1' , '2' , '3'},
+	  { '4' , '5' , '6'},
+	  { '7' , '8' , '9'}
+	};
+
+	std::cout << " " << loc[0][0] << " | " << loc[0][1] << " |" << loc[0][2] << " " << '\n';
+	std::cout << "___|___|___" << '\n';
+	std::cout << " " << loc[1][0] << " | " << loc[1][1] << " |" << loc[1][2] << " " << '\n';
+	std::cout << "___|___|___" << '\n';
+	std::cout << " " << loc[2][0] << " | " << loc[2][1] << " |" << loc[2][2] << " " << '\n';
+	std::cout << "   |   |  " << '\n';
+}
 
 int main ( )
 {
-	std::cout << "  |  |  |  " << '\n';
-	std::cout << "__|__|__|__" << '\n';
-	std::cout << "  |  |  |  " << '\n';
-	std::cout << "__|__|__|__" << '\n';
-	std::cout << "  |  |  |  " << '\n';
-	std::cout << "  |  |  |  " << '\n';
+	TicTacGrid ( );
+
+
 }
