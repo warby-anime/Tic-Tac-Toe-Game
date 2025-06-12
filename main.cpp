1ed33f24ef98108efea4d8a53274c526e22d688e
warby-anime <warby.anime@gmail.com>
2025-06-12 18:28:57 +0100

    added restriction on player input

--- main.cpp
+++ main.cpp
@@ -17,91 +17,94 @@ void Player1Moves ( )
 {
 	std::cout << player1 << " what is your move: ";
 	std::cin >> tokenX;
-	if (tokenX == 1)
+	if (tokenX == 1 && tokenO != 1 && loc[0][0] != 'O')
 	{
 		loc[0][0] = 'X';
 	}
-	if (tokenX == 2)
+	if (tokenX == 2 && tokenO != 2 && loc[0][1] != 'O')
 	{
 		loc[0][1] = 'X';
 	}
-	if (tokenX == 3)
+	if (tokenX == 3 && tokenO != 3 && loc[0][2] != 'O')
 	{
 		loc[0][2] = 'X';
 	}
-	if (tokenX == 4)
+	if (tokenX == 4 && tokenO != 4 && loc[0][0] != 'O')
 	{
 		loc[1][0] = 'X';
 	}
-	if (tokenX == 5)
+	if (tokenX == 5 && tokenO != 5 && loc[1][1] != 'O')
 	{
 		loc[1][1] = 'X';
 	}
-	if (tokenX == 6)
+	if (tokenX == 6 && tokenO != 6 && loc[1][2] != 'O')
 	{
 		loc[1][2] = 'X';
 	}
-	if (tokenX == 7)
+	if (tokenX == 7 && tokenO != 7 && loc[2][0] != 'O')
 	{
 		loc[2][0] = 'X';
 	}
-	if (tokenX == 8)
+	if (tokenX == 8 && tokenO != 8 && loc[2][1] != 'O')
 	{
 		loc[2][1] = 'X';
 	}
-	if (tokenX == 9)
+	if (tokenX == 9 && tokenO != 9 && loc[2][2] != 'O')
 	{
 		loc[2][2] = 'X';
 	}
-	else
+	else if (tokenX < 1 || tokenX > 9  || tokenX == tokenO)
 	{
-		std::cout << player1 << " Invaild input!! ";
+		std::cout << player1 << " Invaild input!! \n";
+
 	}
+	
+	
 }
 
 void Player2Moves ( )
 {
 	std::cout << player2 << " what is your move: ";
 	std::cin >> tokenO;
-	if (tokenO == 1)
+	if (tokenO == 1 && tokenX != 1 && loc[0][0] != 'X')
 	{
 		loc[0][0] = 'O';
 	}
-	if (tokenO == 2)
+	if (tokenO == 2 && tokenX != 2 && loc[0][1] != 'X')
 	{
 		loc[0][1] = 'O';
 	}
-	if (tokenO == 3)
+	if (tokenO == 3 && tokenX != 3 && loc[0][2] != 'X')
 	{
 		loc[0][2] = 'O';
 	}
-	if (tokenO == 4)
+	if (tokenO == 4 && tokenX != 4 && loc[1][0] != 'X')
 	{
 		loc[1][0] = 'O';
 	}
-	if (tokenO == 5)
+	if (tokenO == 5 && tokenX != 5 && loc[1][1] != 'X')
 	{
 		loc[1][1] = 'O';
 	}
-	if (tokenO == 6)
+	if (tokenO == 6 && tokenX != 6 && loc[1][2] != 'X')
 	{
 		loc[1][2] = 'O';
 	}
-	if (tokenO == 7)
+	if (tokenO == 7 && tokenX != 7 && loc[2][0] != 'X')
 	{
 		loc[2][0] = 'O';
 	}
-	if (tokenO == 8)
+	if (tokenO == 8 && tokenX != 8 && loc[2][1] != 'X')
 	{
 		loc[2][1] = 'O';
 	}
-	if (tokenO == 9)
+	if (tokenO == 9 && tokenX != 9 && loc[2][2] != 'X')
 	{
 		loc[2][2] = 'O';
 	}
-	else
+	else if (tokenO < 1 || tokenO > 9)
 	{
-		std::cout << player2 << " Invaild input!! ";
+		std::cout << player2 << " Invaild input!! \n";
 
 	}
 }
